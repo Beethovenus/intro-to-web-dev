b384f4b84d2edc48a60a406ec3eeba0cf746f378
Zach Markle <zamarkle@gmail.com>
2022-03-20 23:07:35 -0400

    week06

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,12 +1,11 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
-const speech = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.`;
+const speech = `I think that right here, I'm just going to write a whole bunch of text.
+It shouldn't be too difficult nor frustrating to write that much, and so I'm just going to do it.
+I'll write on, and on, and on... just to ensure that there'll be enough content for the assignment itself.
+Hopefully this should turn out okay, and I won't have any major issues when it comes to individually sorting everything.
+And if not... well, I guess that's just a part of the subject matter.
+But hey, who's to say this won't ultimately turn out okay?`;
 
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
@@ -33,12 +32,89 @@ function displayLongWords() {
   }
 }
 
+function displayShortWords() {
+  const shortWordsElement = document.getElementById('short-words');
+
+  for(let i=0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    if(word.length < 4) {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      shortWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
+function displayLetterWords() {
+  const letterWordsElement = document.getElementById('letter-words');
+
+  for(let i=0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    if(wordsArray[i].startsWith('w')) {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      letterWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
+function displayLongestWord() {
+  const longestWordElement = document.getElementById('longest-word');
+
+  let longestWord = wordsArray[0];
+
+  for(let i=1; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    if (wordsArray[i].length > longestWord.length) {
+      longestWord = wordsArray[i];
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      longestWordElement.appendChild(wordElement);
+    }
+  }
+}
+
+function displayThirdWords() {
+  const thirdWordsElement = document.getElementById('third-words');
+
+  for(let i=0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    if (wordsArray[i] % 3 == 0) {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      thirdWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
+function displayUncommonWords() {
+  let uncommonWordsElement = document.getElementById('uncommon-words');
+
+  for(let word of wordsArray) {
+    if (word != 'the' && word !='be' && word !='to' && word != 'of' && word != 'and' && word != 'a' && word != 'in' && word != 'that' && word != 'have' && word != 'I') {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      uncommonWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
 // TODO: Define your own functions here!
 
 function displaySpeechStats() {
   document.getElementById('speech').innerText = speech;
 
-  displayLongWords();
+  //displayLongWords();//
+
+  displayShortWords();
+
+  displayLetterWords();
+
+  displayLongestWord();
+
+  displayThirdWords();
+
+  displayUncommonWords();
 
   // TODO: Call your functions here!
 }
